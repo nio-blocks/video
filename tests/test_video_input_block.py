bc3a056edab818267c8ebc3ea41788326fd9c977
Mike Killian <killian441@gmail.com>
2017-07-06 16:38:10 -0600

    Inital Commit

--- tests/test_video_input_block.py
+++ tests/test_video_input_block.py
@@ -1,10 +1,10 @@
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
-from ..example_block import Example
+from ..video_input_block import VideoInput
 
 
-class TestExample(NIOBlockTestCase):
+class TestVideoInput(NIOBlockTestCase):
 
     def test_process_signals(self):
         """Signals pass through block unmodified."""
