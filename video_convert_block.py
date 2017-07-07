31c2744eeb6bbdb26d11f5991abb48824b5bdb2e
Mike Killian <killian441@gmail.com>
2017-07-07 13:27:03 -0600

    Removed some comments, added additional extensions for video_convert

--- video_convert_block.py
+++ video_convert_block.py
@@ -1,11 +1,20 @@
 import numpy
 import cv2
+import io
+from enum import Enum
 from nio.block.base import Block
 from nio.signal.base import Signal
 from nio.util.discovery import discoverable
 from nio.util.threading import spawn
-from nio.properties import StringProperty, BoolProperty, VersionProperty
+from nio.properties import VersionProperty, SelectProperty
 from threading import Event
+
+class Extension(Enum):
+    jpg = '.jpg'
+    bmp = '.bmp'
+    jpeg2000 = '.jp2'
+    png = '.png'
+    tiff = '.tif'
 
 @discoverable
 class VideoConvert(Block):
@@ -13,26 +22,20 @@ class VideoConvert(Block):
     Input raw CV frame (numpy arrays) and output converted frame
     '''
 
-    extension = StringProperty(title='Image extension', default='.jpg')
+    extension = SelectProperty(Extension,
+                              title='Image Extension',
+                              default=Extension.jpg)
     version = VersionProperty('0.0.1')
 
     def process_signals(self, signals):
-        """Overrideable method to be called when signals are delivered.
-        This method will be called by the block router whenever signals
-        are sent to the block. The method should not return the modified
-        signals, but rather call `notify_signals` so that the router
-        can route them properly.
-        Args:
-            signals (list): A list of signals to be processed by the block
-            input_id: The identifier of the input terminal the signals are
-                being delivered to
-        """
         for signal in signals:
             try:
+                image = io.BytesIO()
                 frameSignal = signal.to_dict()
                 frame = frameSignal['frame']
-                ret, image = cv2.imencode(self.extension(), frame)
-                output_sig = {'image':image,'extension':self.extension()}
+                ret, temp_image = cv2.imencode(self.extension().value, frame)
+                image.write(temp_image.tobytes())
+                output_sig = {'image':image,'extension':self.extension().value}
                 self.notify_signals(Signal(output_sig))
             except:
                 self.logger.exception("Failed to execute command")
--- video_save_block.py
+++ video_save_block.py
@@ -18,47 +18,26 @@ class VideoSave(Block):
     version = VersionProperty('0.0.1')
 
     def configure(self, context):
-        """Overrideable method to be called when the block configures.
-        The block creator should call the configure method on the parent,
-        after which it can assume that any parent configuration options present
-        on the block are loaded in as class variables. They can also assume
-        that all functional modules in the service process are loaded and
-        started.
-        Args:
-            context (BlockContext): The context to use to configure the block.
-        Raises:
-            TypeError: If the specified router is not a BlockRouter
-        """
         super().configure(context)
+        #If I move this out of configure, then I could include setup info in the passed signal
         self.videoFile = cv2.VideoWriter(self.filename(), cv2.VideoWriter_fourcc('M','P','E','G'), self.fps(), ast.literal_eval(self.frameSize()))
 
     def process_signals(self, signals):
-        """Overrideable method to be called when signals are delivered.
-        This method will be called by the block router whenever signals
-        are sent to the block. The method should not return the modified
-        signals, but rather call `notify_signals` so that the router
-        can route them properly.
-        Args:
-            signals (list): A list of signals to be processed by the block
-            input_id: The identifier of the input terminal the signals are
-                being delivered to
-        """
         for signal in signals:
             try:
                 #frame = self.frame(signal)
                 frameSignal = signal.to_dict()
                 rawFrame = frameSignal['frame']
-                resizedFrame = cv2.resize(rawFrame,ast.literal_eval(self.frameSize()))
+                if self.frameSize():
+                    resizedFrame = cv2.resize(rawFrame,ast.literal_eval(self.frameSize()))
+                else:
+                    resizedFrame = rawFrame
                 self.videoFile.write(resizedFrame)
             except:
                 self.logger.exception("Failed to execute command")
 
     def stop(self):
-        """Overrideable method to be called when the block stops.
-        The block creator can assume at this point that the service and block
-        router are in "stopping" state. All modules are still available for use
-        in the service process.
-        """
+        '''Not sure this is completely necessary'''
         try:      
             self.logger.debug('Halting VideoSave thread')
             cv2.destroyAllWindows()
